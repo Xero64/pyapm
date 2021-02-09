53849192b508b3693c8c1fea16448909581a90f4
Xero64 <xero64@gmail.com>
2021-02-09 11:22:23 +0200

    Added *.sh to .gitignore and version increase.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyapm",
-    version="0.0.1",
+    version="0.0.2",
     author="Xero64",
     author_email="xero64@gmail.com",
     description="Aerodynamic Panel Method in Python",
